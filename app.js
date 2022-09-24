5ecdc144f173215eeaa648002a9258283dba8cb5
vipker113 <79317962+vipker113@users.noreply.github.com>
2022-09-24 18:42:07 +0700

    pusssshhhhhh

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
+app.use(express.static('resources'));
+app.engine('hbs', exphbs.engine());
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('dangnhap');
+});
+app.get('/trangchu', (req, res) => {
+    res.render('trangchu');
+});
+app.get('/message', (req, res) => {
+    res.render('message');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
