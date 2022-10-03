f9381efee98b483b489618c4dcfd48a314d46c35
letuantai15f <letuantai15f@gmail.com>
2022-10-03 22:23:24 +0700

    tai_model

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const express = require('express');
-
+const cors=require('cors');
+const mongoose=require('mongoose');
+var bodyParser=require('body-parser');
+const morgan=require('morgan');
+const dotenv=require('dotenv');
 const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+const userRouter=require('./routes/user')
+dotenv.config();
+mongoose.connect((process.env.MONGODB_URL),()=>{
+    console.log('MongoDB');
+})
+//
+app.use(bodyParser.json({limit:"50mb"}))
+app.use(cors());
+app.use(morgan("common"));
 app.use(express.static('resources'));
 app.engine('hbs', exphbs.engine());
 app.set('view engine', 'hbs');
-
-
+//configAWS
+const AWS=require('aws-sdk');
+const { response } = require('express');
+const config=new AWS.Config({
+    accessKeyId:'AKIAYN4WGNE3KXQZOA6I',
+    secretAccessKey:'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',
+    region:'ap-southeast-1'
+    
+});
+AWS.config=config;
+const docClient=new AWS.DynamoDB.DocumentClient();
+const tableName="UserAccounts"
+//multer
+const multer=require('multer');
+const upload=multer();
+//getUI
 app.get('/', (req, res) => {
     res.render('login');
 });
@@ -17,6 +45,29 @@ app.get('/home', (req, res) => {
 app.get('/message', (req, res) => {
     res.render('message');
 });
+//signup
+app.post('/signup', upload.fields([]), (req, res) => {
+    const {email,password,name,date,repassword,sex}=req.body;
+    console.log(req.body);
+    const params = {TableName: tableName,Item:{
+      email,password
+    }};
+    const users={TableName:'User',Item:{iduser:'1',name,email,date,sex}};
+
+    
+    docClient.put(params, (err,data) => {
+      if (err) {
+       return res.send('loi');
+    } else{
+        
+        return res.redirect('/');
+    }
+    });
+  
+    
+  });
+
+  app.use("/userAC",userRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
