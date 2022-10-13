15f43782083104cd00e7955fb2367c6729694bfd
letuantai15f <letuantai15f@gmail.com>
2022-10-13 18:00:36 +0700

    tailee fix

--- models/model_messages.js
+++ models/model_messages.js
@@ -1,57 +1,23 @@
 const mongoose = require('mongoose');
-
-const senderSchema = new mongoose.Schema({
-    id: {
-        type: String,
-    },
-    username: {
-        type: String,
-    },
-    avatar: {
-        type: String,
-    }
-});
-
-const receiverSchema = new mongoose.Schema({
-    id: {
-        type: String,
-    },
-    username: {
-        type: String,
-    },
-    avatar: {
-        type: String,
-    }
-});
 const messagesSchema = new mongoose.Schema({
-    id: {
-        type: String,
+    sender:{
+        id:String,
+        username:String,
+        avata: String
     },
-    sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Sender"
+    reciver:{
+        id:String,
+        username:String,
+        avata: String
     },
-    receiver: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"Receiver"
-    },
-    text: {
-        type: String,
-    },
-    file: {
-        type: Buffer,
-    },
-    // createdAt: { 
-    //     type: timestamp,
-    // },
-    // updateAt: {
-    //     type: timestamp,
-    // },
-    // deleteAt: {
-    //     type: timestamp,
-    // }
+    text:String,
+    file:{data:Buffer,contenType: String,fileName:String},
+    // createdAt: {type:Number,default:Date.now},
+    // updateAt: {type:Number,default:null},
+    // deleteAt: {type:Number,default:null},
+     
+    
+
 });
-let Messages = mongoose.model('Messages', messagesSchema);
-let Sender = mongoose.model('Sender', senderSchema);
-let Receiver = mongoose.model('Receiver', receiverSchema);
-module.exports = { Messages, Sender, Receiver };
+let message = mongoose.model('message', messagesSchema);
+module.exports = {message};
--- routes/message.js
+++ routes/message.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const messageRouter = express.Router();
-const { Message } = require("../models/model_messages");
+const { message}=require("../models/model_messages")
 const { User } = require("../models/model");
 
 const multer = require("multer");
@@ -8,8 +8,16 @@ const upload = multer();
 messageRouter.get('/',async(req, res) =>{
     console.log(1);
         const user=await User.findOne({"account.email":"letuantai15f@gmail.com"});
-    console.log(user);
-    res.render('message',{dataimg:user.avata});
+        const mess= await message.find({});
+        console.log(mess);
+        const messconact={
+            avata:user.avata,
+            avtreciver:mess[0].reciver.avata,
+            text:mess[0].text,
+            username:mess[0].reciver.username
+        }
+        console.log(messconact);
+    res.render('message',{dataimg:messconact});
 
 });
 
