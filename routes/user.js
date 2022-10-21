b24771b4da4447f98780c188e53336383b1b9d31
letuantai15f <letuantai15f@gmail.com>
2022-10-21 18:41:36 +0700

    tai_login

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,7 @@ const userRouter = express.Router();
 const { User } = require("../models/model");
 const multer = require("multer");
 const upload = multer();
-const jwt = require("jsonwebtoken");
-const firebase = require("../config/firebase");
-const notify = require("alert");
+const jwt=require("jsonwebtoken");
 
 userRouter.post("/signup", upload.fields([]), async (req, res) => {
   const { username, email, password, name, date, repassword, gender } =
@@ -18,21 +16,13 @@ userRouter.post("/signup", upload.fields([]), async (req, res) => {
       "https://thtuan2-130821.s3.ap-southeast-1.amazonaws.com/zyro-image.png",
     account: { email, password },
   };
-
-  const user = await User.findOne({ "account.email": email });
-  if (user == null) {
-    firebase.addUser(email, password);
+  try {
     const tuser = new User(newUser);
+    console.log(tuser);
     const saveUser = await tuser.save();
-    var status = "alert alert-success";
-    var notify = "Đăng kí thành công";
-    var dataStatus = { status, notify };
-    res.render("login", { message: dataStatus });
-  } else {
-    var status = "alert alert-danger";
-    var notify = "Đăng kí thất bại. Tài khoản đã tồn tại";
-    var dataStatus = { status, notify };
-    res.render("login", { message: dataStatus });
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 userRouter.post("/login", upload.fields([]), async (req, res) => {
@@ -45,6 +35,17 @@ userRouter.post("/login", upload.fields([]), async (req, res) => {
     if (user.account.password == password) {
     var token= jwt.sign({id:user._id,username:user.email},process.env.JWT_KEY,{expiresIn:"1h"});
       res.cookie('token', token, { maxAge: 900000, httpOnly: true });
+<<<<<<< HEAD
+=======
+
+      return res.redirect("/message");
+    }
+    else{
+      res.redirect("/");
+    }
+  }
+  });
+>>>>>>> parent of e59db89 (login_regist)
 
       return res.redirect("/message");
     }
