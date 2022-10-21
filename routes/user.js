23e500f3cc9a6c33d8467b6f618306315d94bbeb
letuantai15f <letuantai15f@gmail.com>
2022-10-21 19:24:03 +0700

    tailogin

--- routes/user.js
+++ routes/user.js
@@ -4,55 +4,45 @@ const { User } = require("../models/model");
 const multer = require("multer");
 const upload = multer();
 const jwt=require("jsonwebtoken");
+const firebase = require("../config/firebase");
+
 
 userRouter.post("/signup", upload.fields([]), async (req, res) => {
-  const { username, email, password, name, date, repassword, gender } =
-    req.body;
+  const { username, email, password, name, date, repassword, gender } =req.body
   const newUser = {
     username,
     gender,
     date,
     avata:
       "https://thtuan2-130821.s3.ap-southeast-1.amazonaws.com/zyro-image.png",
-    account: { email, password },
+    account: { email},
   };
-  try {
+  
+  const user = await User.findOne({ "account.email": email });
+  if (user == null) {
+    try{
+    firebase.addUser(email, password);
     const tuser = new User(newUser);
     console.log(tuser);
     const saveUser = await tuser.save();
     res.redirect("/");
   } catch (err) {
     res.status(500).json(err);
+    var status = "alert alert-success";
+    var notify = "Đăng kí thành công";
+    var dataStatus = { status, notify };
+    res.render("login", { message: dataStatus });
+  }} else {
+    var status = "alert alert-danger";
+    var notify = "Đăng kí thất bại. Tài khoản đã tồn tại";
+    var dataStatus = { status, notify };
+    res.render("login", { message: dataStatus });
   }
 });
 userRouter.post("/login", upload.fields([]), async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ "account.email": email });
-  if (user == null) {
-    
-    return res.render("login",{dataText:"Email hoặc mật khẩu không đúng ?"});
-  } else {
-    if (user.account.password == password) {
-    var token= jwt.sign({id:user._id,username:user.email},process.env.JWT_KEY,{expiresIn:"1h"});
-      res.cookie('token', token, { maxAge: 900000, httpOnly: true });
-<<<<<<< HEAD
-=======
-
-      return res.redirect("/message");
-    }
-    else{
-      res.redirect("/");
-    }
-  }
-  });
->>>>>>> parent of e59db89 (login_regist)
-
-      return res.redirect("/message");
-    }
-    else{
-      res.redirect("/");
-    }
-  }
-  });
+  firebase.authenticate(email, password, req, res);
+});
 
 module.exports = userRouter;
